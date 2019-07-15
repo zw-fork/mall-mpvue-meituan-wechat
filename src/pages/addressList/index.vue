87af57d93e2a13fce31bccf84d9f13bb8f0f4e8e
zw <1094660626@qq.com>
2019-07-15 15:06:31 +0800

    data

--- src/pages/addressList/index.vue
+++ src/pages/addressList/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="list-c">
-      <div class="item" v-for="(item, index) in myAddress" :key="index" v-if="item.wxAddress && item.wxAddress.name == shopInfo.wxAddress.name">
+      <div class="item" v-for="(item, index) in myAddress" :key="index">
         <div class="i-l" @touchstart="showDeleteButton(item.id)" @touchend="clearLoop(item.id)">
           <div class="user-info">
             <span class="s-l">{{item.name}}({{item.gender===1 ? '先生' : '女士'}})</span>
@@ -20,74 +20,76 @@
 </template>
 
 <script>
-import {addressList} from './data'
+import { addressList } from "./data";
 import { mapState, mapActions, mapMutations, mapGetters } from "vuex";
 
 export default {
   data() {
     return {
-     communityId: undefined,
-     showModal: false,
-     Loop: undefined
-    }
+      communityId: undefined,
+      showModal: false,
+      Loop: undefined
+    };
   },
   computed: {
     ...mapState("address", ["myAddress"]),
     ...mapState("user", ["userInfo"]),
-    ...mapState("shoppingCart", ["shopInfo"]),
+    ...mapState("shoppingCart", ["shopInfo"])
   },
   methods: {
-    ...mapActions("address", ["getAddressDataAction", "deleteUserAddressDataAction"]),
+    ...mapActions("address", [
+      "getAddressDataAction",
+      "deleteUserAddressDataAction"
+    ]),
     ...mapActions("user", ["updateDefaultAddress"]),
     showDeleteButton(addressId) {
-    var that = this
-    this.Loop=setTimeout(function(){
-      that.showModal = true
-              wx.showModal({
-          title: '删除？',
-          content: '确定要删除当前选中的地址？',
-          confirmColor: '#FFC24A',
+      var that = this;
+      this.Loop = setTimeout(function() {
+        that.showModal = true;
+        wx.showModal({
+          title: "删除？",
+          content: "确定要删除当前选中的地址？",
+          confirmColor: "#FFC24A",
           success: function(res) {
             if (res.confirm) {
-             that.deleteUserAddressDataAction({addressId})
+              that.deleteUserAddressDataAction({ addressId });
             }
-        }
-      })
-    },800);
-    return false;
-},
-clearLoop(addressId) {
-    clearTimeout(this.Loop);
-    if(!this.showModal){
-      this.updateDefaultAddress2(addressId)
-    }
-    this.showModal = false
-    return false;
-},
+          }
+        });
+      }, 800);
+      return false;
+    },
+    clearLoop(addressId) {
+      clearTimeout(this.Loop);
+      if (!this.showModal) {
+        this.updateDefaultAddress2(addressId);
+      }
+      this.showModal = false;
+      return false;
+    },
     addClick(id) {
       if (id) {
-        wx.navigateTo({url: '/pages/addAddress/main?id=' + id})
+        wx.navigateTo({ url: "/pages/addAddress/main?id=" + id });
       } else {
-        wx.navigateTo({url: '/pages/addAddress/main'})
+        wx.navigateTo({ url: "/pages/addAddress/main" });
       }
     },
     updateDefaultAddress2(addressId) {
-      var wechat = {}
-      wechat.openid = this.userInfo.openid
-      wechat.addressModel = {}
-      wechat.addressModel.id = addressId
-      this.updateDefaultAddress({wechat})
+      var wechat = {};
+      wechat.openid = this.userInfo.openid;
+      wechat.addressModel = {};
+      wechat.addressModel.id = addressId;
+      this.updateDefaultAddress({ wechat });
     }
   },
   mounted() {
-    var openid = this.userInfo.openid
-    this.getAddressDataAction({'openid': openid})
+    var openid = this.userInfo.openid;
+    this.getAddressDataAction({ openid: openid });
   },
-  onLoad(options) 
-  {
-    this.communityId=options.communityId;
+  onLoad(options) {
+    this.communityId = options.communityId;
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -111,7 +113,7 @@ clearLoop(addressId) {
         }
         .user-info {
           display: flex;
-          font-weight:bold;
+          font-weight: bold;
           font-size: 30rpx;
           .s-l {
             color: $textBlack-color;
@@ -126,7 +128,7 @@ clearLoop(addressId) {
           }
         }
       }
-      i{
+      i {
         font-size: 30rpx;
         color: $textDarkGray-color;
       }
@@ -138,7 +140,7 @@ clearLoop(addressId) {
     justify-content: center;
     height: 88rpx;
     background-color: $theme-color;
-    border-top: 2rpx solid  $spLine-color;
+    border-top: 2rpx solid $spLine-color;
     position: fixed;
     bottom: 0;
     left: 0;
