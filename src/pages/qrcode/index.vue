eaf6434cb7ee91eb724d82c09f940711b489e768
zw <1094660626@qq.com>
2019-08-06 18:00:17 +0800

    data

--- src/pages/qrcode/index.vue
+++ src/pages/qrcode/index.vue
@@ -1,39 +1,32 @@
 <template>
-  <div class="rr">
-    <canvas
-      style="width: 400px; height: 400px;"
-      class="canvas"
-      canvas-id="canvas"
-      bindlongtap="save"
-    ></canvas>
-  </div>
+	<div class="qrcode-wrap">
+		<div class="qrcode-container">
+			<image class="qrcode-img" :src="qrcodeURL"></image>
+		</div>
+	</div>
 </template>
 
  <script>
-import QRCode from "../../utils/weapp-qrcode.js";
+const QR = require('../../utils/weapp-qrcode.js')
+
 export default {
   data() {
     return {
       shopId: undefined,
-      userInfo: {}
+      userInfo: {},
+      qrcodeURL: "",
+      codeText: "github.com/Pudon",
     };
   },
   methods: {
     qrcode() {
       var text = encodeURIComponent(wx.getStorageSync("sessionId"));
-      var qrcode = new QRCode("canvas", {
-        // usingIn: this,
-        text: text,
-        padding: 12,
-        colorDark: "#000000",
-        colorLight: "#ffffff",
-        correctLevel: QRCode.CorrectLevel.H,
-        callback: res => {
-          // 生成二维码的临时文件
-          console.log(res.path);
-        }
-      });
-      console.log(qrcode);
+    var imgData = QR.drawImg(text, {
+      typeNumber: 4,
+      errorCorrectLevel: 'M',
+      size: 500
+    })
+    this.qrcodeURL = imgData
     }
   },
   mounted() {
@@ -43,7 +36,38 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.rr {
-  top: 60%;
+.container {
+  background: #f5f5f5;
+}
+.qrcode-wrap {
+  background: #fff;
+  border-radius: 33rpx;
+}
+.qrcode-container {
+  padding-top: 80rpx;
+  padding-bottom: 50rpx;
+  text-align: center;
+}
+.qrcode-img {
+  width: 500rpx;
+  height: 500rpx;
+}
+.text-container {
+  padding: 50rpx;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 2rpx solid #f0f0f0;
+}
+.text-input {
+  border: 2rpx solid #f5f5f5;
+  border-radius: 10rpx;
+  margin-right: 10rpx;
+  padding: 20rpx;
+}
+.change-btn {
+  width: 200rpx;
+  color: #fff;
+  padding: 0 50rpx;
 }
 </style>
